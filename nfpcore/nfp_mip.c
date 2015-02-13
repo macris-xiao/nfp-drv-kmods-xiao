4d218c671481ffab3ef351a40155ad46bfa93f8b
Jason S. McMullan <jason.mcmullan@netronome.com>
2015-02-12 19:18:25 -0500

    nfpcore/nfp_mip: Scan DRAM, not PCIe CTM, for MIP on the NFP6000
    If the 'nfp_mip' or nfp_rtsym' APIs were called when no firmware was loaded,
    the PCIe CTM would be scanned for a MIP, instead of desired location of the
    DRAM on EMU Island 24.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@netronome.com>

--- nfpcore/nfp_mip.c
+++ nfpcore/nfp_mip.c
@@ -175,12 +175,13 @@ int __nfp_mip_location(struct nfp_device *dev,
 	uint64_t mip_off = 0;
 	struct nfp_cpp *cpp = nfp_device_cpp(dev);
 	struct nfp_mip mip;
+	uint32_t model = nfp_cpp_model(nfp_device_cpp(dev));
 
 	/* First see if we can get it from the nfp.nffw resource */
 	if (nfp_nffw_info_acquire(dev) == 0) {
 		int mu_lsb = -1;
 
-		if (NFP_CPP_MODEL_IS_6000(nfp_cpp_model(nfp_device_cpp(dev))))
+		if (NFP_CPP_MODEL_IS_6000(model)) 
 			mu_lsb = nfp_mip_nfp6000_mu_locality_lsb(dev);
 		else
 			mu_lsb = 38; /* Assume 40-bit addressing */
@@ -216,6 +217,8 @@ int __nfp_mip_location(struct nfp_device *dev,
 		     mip_off += 4096) {
 			uint32_t cpp_id = NFP_CPP_ID(NFP_CPP_TARGET_MU,
 						     NFP_CPP_ACTION_RW, 0);
+			if (NFP_CPP_MODEL_IS_6000(model))
+				cpp_id |= 24;
 			retval = nfp_cpp_read(cpp, cpp_id,
 					      mip_off,
 					      &mip, sizeof(mip));
