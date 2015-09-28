macris-xiao/nfp-drv-kmods-xiao

commit 4b4460ea12c0ad0337e1fd4f76aa20c85882a292

nfp_net_common: improve setting device mac
netdev->dev_addr is stored in network order and NFP
expects registers to be written with the value in
little endian.  Also lowest two bytes need to be
put into two highest on NFP.

All this is a bit strange so lets put this piece of
code into a separate function.

Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>

src/nfp_net_common.c
@@ -2107,6 +2107,18 @@ void nfp_net_coalesce_write_cfg(struct nfp_net *nn)
 }
 
 /**
+ * nfp_net_write_mac_addr - Write mac address to device registers
+ * We do a bit of byte swapping dance because firmware is LE.
+ */
+static void nfp_net_write_mac_addr(struct nfp_net *nn, const u8 *mac)
+{
+	writel(get_unaligned_be32(nn->netdev->dev_addr),
+	       nn->ctrl_bar + NFP_NET_CFG_MACADDR);
+	writew(get_unaligned_be16(nn->netdev->dev_addr + 4),
+	       nn->ctrl_bar + NFP_NET_CFG_MACADDR + 6);
+}
+
+/**
  * nfp_net_netdev_open - Called when the device is upped
  */
 static int nfp_net_netdev_open(struct net_device *netdev)
@@ -2193,10 +2205,7 @@ static int nfp_net_netdev_open(struct net_device *netdev)
 		  nn->num_rx_rings == 64 ? 0xffffffffffffffffULL
 		  : ((u64)1 << nn->num_rx_rings) - 1);
 
-	nn_writel(nn->ctrl_bar, NFP_NET_CFG_MACADDR,
-		  cpu_to_be32(*(u32 *)nn->netdev->dev_addr));
-	nn_writel(nn->ctrl_bar, NFP_NET_CFG_MACADDR + 4,
-		  cpu_to_be32(*(u32 *)(nn->netdev->dev_addr + 4)));
+	nfp_net_write_mac_addr(nn, netdev->dev_addr);
 
 	nn_writel(nn->ctrl_bar, NFP_NET_CFG_MTU, netdev->mtu);
 	nn_writel(nn->ctrl_bar, NFP_NET_CFG_FLBUFSZ, nn->fl_bufsz);
@@ -2641,11 +2650,7 @@ int nfp_net_netdev_init(struct net_device *netdev)
 	nn->cap = nn_readl(nn->ctrl_bar, NFP_NET_CFG_CAP);
 	nn->max_mtu = nn_readl(nn->ctrl_bar, NFP_NET_CFG_MAX_MTU);
 
-	/* Write the MAC address */
-	nn_writel(nn->ctrl_bar, NFP_NET_CFG_MACADDR,
-		  cpu_to_be32(*(u32 *)nn->netdev->dev_addr));
-	nn_writel(nn->ctrl_bar, NFP_NET_CFG_MACADDR + 4,
-		  cpu_to_be32(*(u32 *)(nn->netdev->dev_addr + 4)));
+	nfp_net_write_mac_addr(nn, nn->netdev->dev_addr);
 
 	/* Set default MTU and Freelist buffer size */
 	if (nn->max_mtu < NFP_NET_DEFAULT_MTU)

commit c676e1b8b8c5a40901d2e0d29ec02dc1ea711a7b

nfp_net_common: Correct issue where MAC was not updated on NFP3200
The NFP3200 does not correctly handle 16-bit writes to the control
BAR. This change reverts the behavior to the original two 32-bit
writes to the control BAR, which works on both NFP3200 and NFP6000
devices.

Fixes: 4b4460ea12c0ad0337e1fd4f76aa20c85882a292 (nfp_net_common: improve setting device mac)
Signed-off-by: Jason S. McMullan <jason.mcmullan@netronome.com>

src/nfp_net_common.c
@@ -2152,10 +2152,10 @@ void nfp_net_coalesce_write_cfg(struct nfp_net *nn)
  */
 static void nfp_net_write_mac_addr(struct nfp_net *nn, const u8 *mac)
 {
-	writel(get_unaligned_be32(nn->netdev->dev_addr),
-	       nn->ctrl_bar + NFP_NET_CFG_MACADDR);
-	writew(get_unaligned_be16(nn->netdev->dev_addr + 4),
-	       nn->ctrl_bar + NFP_NET_CFG_MACADDR + 6);
+	nn_writel(nn, NFP_NET_CFG_MACADDR + 0,
+		  get_unaligned_be32(nn->netdev->dev_addr));
+	nn_writel(nn, NFP_NET_CFG_MACADDR + 4,
+		  get_unaligned_be16(nn->netdev->dev_addr + 4) << 16);
 }
 
 /**