macris-xiao/nfp-drv-kmods-xiao

commit 9ac8d2b1f1cbacb84fdebd32871ea2ae1faace6b

nfp_net: disable TSO by default even if advertised by the FW
Signed-off-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

src/nfp_net_common.c
@@ -2433,10 +2433,6 @@ int nfp_net_netdev_init(struct net_device *netdev)
 		netdev->hw_features |= NETIF_F_SG;
 		nn->ctrl |= NFP_NET_CFG_CTRL_SCATTER | NFP_NET_CFG_CTRL_GATHER;
 	}
-	if (nn->cap & NFP_NET_CFG_CTRL_LSO) {
-		netdev->hw_features |= NETIF_F_TSO | NETIF_F_TSO6;
-		nn->ctrl |= NFP_NET_CFG_CTRL_LSO;
-	}
 	if (nn->cap & NFP_NET_CFG_CTRL_RSS) {
 		netdev->hw_features |= NETIF_F_RXHASH | NETIF_F_NTUPLE;
 		nn->ctrl |= NFP_NET_CFG_CTRL_RSS;
@@ -2457,6 +2453,10 @@ int nfp_net_netdev_init(struct net_device *netdev)
 		nn->ctrl |= NFP_NET_CFG_CTRL_TXVLAN;
 	}
 	netdev->features = netdev->hw_features;
+
+	/* Advertise but disable TSO by default. */
+	if (nn->cap & NFP_NET_CFG_CTRL_LSO)
+		netdev->hw_features |= NETIF_F_TSO | NETIF_F_TSO6;
 
 	/* Allow L2 Broadcast through by default, if supported */
 	if (nn->cap & NFP_NET_CFG_CTRL_L2BC)

commit f245ddd5f4cb2775b92fce0c7c6346faf652f3ff

nfp_net_common: fix LSO over encapsulated and tagged links
commit 9ac8d2b1f1cb ("nfp_net: disable TSO by default even
if advertised by the FW")  changed the place where the TSO
feature bits are set.  Unfortunately it moved it after the
vlan_features setting which effectively disables TSO over
vlan tagged links. Subsequently encapsulation support was
added and the feature settings were inserted after the TSO
setting in hope that it will suffice the get TSO working
over encapsulated link.  Unfortunately we never set encap
GSO types on netdev->features so TSO will not work here
either.

Let's put things back in order and explicitly clear TSO
flags from netdev->features.  It's definitely a less
clever way but IMO is more explicit about the intent than
reordering.

While at it remove masking of hw_enc_features and simply
set it to hw_features.  Masking is not necessary since all
the features set until this point are safe to set for
encapsulated links as well.

Fixes: 9ac8d2b1f1cb ("nfp_net: disable TSO by default even if advertised by the FW")
Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Reviewed-by: Simon Horman <simon.horman@netronome.com>

src/nfp_net_common.c
@@ -2445,9 +2445,22 @@ int nfp_net_netdev_init(struct net_device *netdev)
 		netdev->hw_features |= NETIF_F_SG;
 		nn->ctrl |= NFP_NET_CFG_CTRL_GATHER;
 	}
+	if ((nn->cap & NFP_NET_CFG_CTRL_LSO) && nn->fw_ver.major > 2) {
+		netdev->hw_features |= NETIF_F_TSO | NETIF_F_TSO6;
+		nn->ctrl |= NFP_NET_CFG_CTRL_LSO;
+	}
 	if (nn->cap & NFP_NET_CFG_CTRL_RSS) {
 		netdev->hw_features |= NETIF_F_RXHASH;
 		nn->ctrl |= NFP_NET_CFG_CTRL_RSS;
+	}
+	if (nn->cap & NFP_NET_CFG_CTRL_VXLAN &&
+	    nn->cap & NFP_NET_CFG_CTRL_NVGRE) {
+		if (nn->cap & NFP_NET_CFG_CTRL_LSO)
+			netdev->hw_features |= NETIF_F_GSO_GRE |
+					       NETIF_F_GSO_UDP_TUNNEL;
+		nn->ctrl |= NFP_NET_CFG_CTRL_VXLAN | NFP_NET_CFG_CTRL_NVGRE;
+
+		netdev->hw_enc_features = netdev->hw_features;
 	}
 
 	netdev->vlan_features = netdev->hw_features;
@@ -2464,26 +2477,7 @@ int nfp_net_netdev_init(struct net_device *netdev)
 	netdev->features = netdev->hw_features;
 
 	/* Advertise but disable TSO by default. */
-	if ((nn->cap & NFP_NET_CFG_CTRL_LSO) && nn->fw_ver.major > 2)
-		netdev->hw_features |= NETIF_F_TSO | NETIF_F_TSO6;
-
-	if (nn->cap & NFP_NET_CFG_CTRL_VXLAN &&
-	    nn->cap & NFP_NET_CFG_CTRL_NVGRE) {
-		if (nn->cap & NFP_NET_CFG_CTRL_LSO)
-			netdev->hw_features |= NETIF_F_GSO_GRE |
-					       NETIF_F_GSO_UDP_TUNNEL;
-
-		/* Advertise features we support on encapsulated packets */
-		netdev->hw_enc_features =
-			netdev->hw_features & (NETIF_F_IP_CSUM |
-					       NETIF_F_IPV6_CSUM |
-					       NETIF_F_TSO |
-					       NETIF_F_TSO6 |
-					       NETIF_F_GSO_GRE |
-					       NETIF_F_GSO_UDP_TUNNEL);
-
-		nn->ctrl |= NFP_NET_CFG_CTRL_VXLAN | NFP_NET_CFG_CTRL_NVGRE;
-	}
+	netdev->features &= ~(NETIF_F_TSO | NETIF_F_TSO6);
 
 	/* Allow L2 Broadcast through by default, if supported */
 	if (nn->cap & NFP_NET_CFG_CTRL_L2BC)