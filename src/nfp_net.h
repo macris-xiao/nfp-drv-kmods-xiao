macris-xiao/nfp-drv-kmods-xiao

commit 1ee3c5600ccf230690900962baf796e6ee0c56bf

nfp_net: add RX encapsulation offload
Add configuration of VXLAN ports and reporting of inner
checksum OK.

Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>

src/nfp_net.h
@@ -112,6 +112,9 @@ struct nfp_net;
 
 #define NFP_NET_FL_BATCH	16	/* Add freelist in this Batch size */
 
+/* Offload definitions */
+#define NFP_NET_N_VXLAN_PORTS	(NFP_NET_CFG_VXLAN_SZ / sizeof(__be16))
+
 /* Debug support */
 #define NFP_NET_DUMP_TX_MIN	1000
 #define NFP_NET_DUMP_TX_MAX	(NFP_NET_DUMP_TX_MIN + NFP_NET_MAX_TX_RINGS - 1)
@@ -365,6 +368,7 @@ struct nfp_net_r_vector {
 	u64 rx_pkts;
 	u64 rx_bytes;
 	u64 hw_csum_rx_ok;
+	u64 hw_csum_rx_inner_ok;
 	u64 hw_csum_rx_error;
 
 	struct u64_stats_sync tx_sync;
@@ -433,6 +437,7 @@ struct nfp_net_r_vector {
  * @rx_coalesce_max_frames RX interrupt moderation frame count parameter
  * @tx_coalesce_usecs      TX interrupt moderation usecs delay parameter
  * @tx_coalesce_max_frames TX interrupt moderation frame count parameter
+ * @vxlan_ports:	VXLAN ports for RX inner csum offload communicated to HW
  * @qcp_cfg:            Pointer to QCP queue used for configuration notification
  * @ctrl_bar:           Pointer to mapped control BAR
  * @tx_bar:             Pointer to mapped TX queues
@@ -518,6 +523,8 @@ struct nfp_net {
 	u32 rx_coalesce_max_frames;
 	u32 tx_coalesce_usecs;
 	u32 tx_coalesce_max_frames;
+
+	__be16 vxlan_ports[NFP_NET_N_VXLAN_PORTS];
 
 	u8 __iomem *qcp_cfg;
 

src/nfp_net_common.c
@@ -1179,15 +1179,25 @@ static void nfp_net_rx_csum(struct nfp_net *nn, struct nfp_net_r_vector *r_vec,
 		return;
 	}
 
-	if (!(rxd->rxd.flags & PCIE_DESC_RX_TCP_CSUM_OK) &&
-	    !(rxd->rxd.flags & PCIE_DESC_RX_UDP_CSUM_OK))
-		return;
+	/* Assume that the firmware will never report inner CSUM_OK unless outer
+	 * L4 headers were successfully parsed. FW will always report zero UDP
+	 * checksum as CSUM_OK.
+	 */
+	if (rxd->rxd.flags & PCIE_DESC_RX_TCP_CSUM_OK ||
+	    rxd->rxd.flags & PCIE_DESC_RX_UDP_CSUM_OK) {
+		compat_incr_checksum_unnecessary(skb, false);
+		u64_stats_update_begin(&r_vec->rx_sync);
+		r_vec->hw_csum_rx_ok++;
+		u64_stats_update_end(&r_vec->rx_sync);
+	}
 
-	skb->ip_summed = CHECKSUM_UNNECESSARY;
-
-	u64_stats_update_begin(&r_vec->rx_sync);
-	r_vec->hw_csum_rx_ok++;
-	u64_stats_update_end(&r_vec->rx_sync);
+	if (rxd->rxd.flags & PCIE_DESC_RX_I_TCP_CSUM_OK ||
+	    rxd->rxd.flags & PCIE_DESC_RX_I_UDP_CSUM_OK) {
+		compat_incr_checksum_unnecessary(skb, true);
+		u64_stats_update_begin(&r_vec->rx_sync);
+		r_vec->hw_csum_rx_inner_ok++;
+		u64_stats_update_end(&r_vec->rx_sync);
+	}
 }
 
 /**
@@ -2371,6 +2381,60 @@ nfp_net_features_check(struct sk_buff *skb, struct net_device *dev,
 }
 #endif
 
+#if COMPAT__HAVE_VXLAN_OFFLOAD
+/**
+ * nfp_net_set_vxlan_port() - set vxlan port in SW and HW
+ * @nn:   NFP Net device to reconfigure
+ * @idx:  Index into the port table where new port should be written
+ * @port: UDP port to configure (pass zero to remove VXLAN port)
+ */
+static void nfp_net_set_vxlan_port(struct nfp_net *nn, int idx, __be16 port)
+{
+	int i;
+
+	BUILD_BUG_ON(NFP_NET_N_VXLAN_PORTS & 1);
+
+	nn->vxlan_ports[idx] = port;
+
+	for (i = 0; i < NFP_NET_N_VXLAN_PORTS; i += 2)
+		nn_writel(nn, NFP_NET_CFG_VXLAN_PORT,
+			  be16_to_cpu(nn->vxlan_ports[i + 1]) << 16 |
+			  be16_to_cpu(nn->vxlan_ports[i]));
+
+	nfp_net_reconfig(nn, NFP_NET_CFG_UPDATE_VXLAN);
+}
+
+static void nfp_net_add_vxlan_port(struct net_device *netdev,
+				   sa_family_t sa_family, __be16 port)
+{
+	struct nfp_net *nn = netdev_priv(netdev);
+	int i, free_idx = -1;
+
+	for (i = 0; i < NFP_NET_N_VXLAN_PORTS; i++) {
+		if (nn->vxlan_ports[i] == port)
+			return;
+		if (!nn->vxlan_ports[i])
+			free_idx = i;
+	}
+
+	if (free_idx == -1)
+		return;
+
+	nfp_net_set_vxlan_port(nn, free_idx, port);
+}
+
+static void nfp_net_del_vxlan_port(struct net_device *netdev,
+				   sa_family_t sa_family, __be16 port)
+{
+	struct nfp_net *nn = netdev_priv(netdev);
+	int i;
+
+	for (i = 0; i < NFP_NET_N_VXLAN_PORTS; i++)
+		if (nn->vxlan_ports[i] == port)
+			nfp_net_set_vxlan_port(nn, i, 0);
+}
+#endif /* COMPAT__HAVE_VXLAN_OFFLOAD */
+
 static struct net_device_ops nfp_net_netdev_ops = {
 	.ndo_open		= nfp_net_netdev_open,
 	.ndo_stop		= nfp_net_netdev_close,
@@ -2383,6 +2447,10 @@ static struct net_device_ops nfp_net_netdev_ops = {
 	.ndo_set_features	= nfp_net_set_features,
 #if COMPAT__HAVE_NDO_FEATURES_CHECK
 	.ndo_features_check	= nfp_net_features_check,
+#endif
+#if COMPAT__HAVE_VXLAN_OFFLOAD
+	.ndo_add_vxlan_port     = nfp_net_add_vxlan_port,
+	.ndo_del_vxlan_port     = nfp_net_del_vxlan_port,
 #endif
 };
 

src/nfp_net_compat.h
@@ -281,6 +281,17 @@ int compat_dma_set_mask_and_coherent(struct device *dev, u64 mask)
 #define skb_vlan_tag_get(skb)		vlan_tx_tag_get(skb)
 #endif
 
+static inline
+void compat_incr_checksum_unnecessary(struct sk_buff *skb, bool encap)
+{
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 18, 0)
+	skb->ip_summed = CHECKSUM_UNNECESSARY;
+	skb->encapsulation = encap;
+#else
+	__skb_incr_checksum_unnecessary(skb);
+#endif
+}
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 19, 0)
 static inline void napi_complete_done(struct napi_struct *n, int work_done)
 {

src/nfp_net_ctrl.h
@@ -137,6 +137,7 @@
 #define   NFP_NET_CFG_UPDATE_L2SWITCH     (0x1 <<  6) /* Switch changes */
 #define   NFP_NET_CFG_UPDATE_RESET        (0x1 <<  7) /* Update due to FLR */
 #define   NFP_NET_CFG_UPDATE_IRQMOD       (0x1 <<  8) /* IRQ mod change */
+#define   NFP_NET_CFG_UPDATE_VXLAN	  (0x1 <<  9) /* VXLAN port change */
 #define   NFP_NET_CFG_UPDATE_ERR          (0x1 << 31) /* A error occurred */
 #define NFP_NET_CFG_TXRS_ENABLE         0x0008
 #define NFP_NET_CFG_RXRS_ENABLE         0x0010
@@ -189,6 +190,9 @@
  */
 #define NFP_NET_CFG_RX_OFFSET		0x0050
 #define NFP_NET_CFG_RX_OFFSET_DYNAMIC		0	/* Prepend mode */
+
+#define NFP_NET_CFG_VXLAN_PORT		0x0060
+#define NFP_NET_CFG_VXLAN_SZ		0x0008
 
 /**
  * 64B reserved for future use (0x0080 - 0x00c0)

src/nfp_net_ethtool.c
@@ -111,7 +111,7 @@ static const struct _nfp_net_et_stats nfp_net_et_stats[] = {
 
 #define NN_ET_GLOBAL_STATS_LEN ARRAY_SIZE(nfp_net_et_stats)
 #define NN_ET_RVEC_STATS_LEN (nn->num_r_vecs * 3)
-#define NN_ET_RVEC_GATHER_STATS 6
+#define NN_ET_RVEC_GATHER_STATS 7
 #define NN_ET_QUEUE_STATS_LEN ((nn->num_tx_rings + nn->num_rx_rings) * 2)
 #define NN_ET_STATS_LEN (NN_ET_GLOBAL_STATS_LEN + NN_ET_RVEC_GATHER_STATS + \
 			 NN_ET_RVEC_STATS_LEN + NN_ET_QUEUE_STATS_LEN)
@@ -207,6 +207,8 @@ static void nfp_net_get_strings(struct net_device *netdev,
 		}
 		strncpy(p, "hw_rx_csum_ok", ETH_GSTRING_LEN);
 		p += ETH_GSTRING_LEN;
+		strncpy(p, "hw_rx_csum_inner_ok", ETH_GSTRING_LEN);
+		p += ETH_GSTRING_LEN;
 		strncpy(p, "hw_rx_csum_err", ETH_GSTRING_LEN);
 		p += ETH_GSTRING_LEN;
 		strncpy(p, "hw_tx_csum", ETH_GSTRING_LEN);
@@ -275,17 +277,18 @@ static void nfp_net_get_stats(struct net_device *netdev,
 			start = u64_stats_fetch_begin(&nn->r_vecs[j].rx_sync);
 			data[i++] = nn->r_vecs[j].rx_pkts;
 			tmp[0] = nn->r_vecs[j].hw_csum_rx_ok;
-			tmp[1] = nn->r_vecs[j].hw_csum_rx_error;
+			tmp[1] = nn->r_vecs[j].hw_csum_rx_inner_ok;
+			tmp[2] = nn->r_vecs[j].hw_csum_rx_error;
 		} while (u64_stats_fetch_retry(&nn->r_vecs[j].rx_sync, start));
 
 		do {
 			start = u64_stats_fetch_begin(&nn->r_vecs[j].tx_sync);
 			data[i++] = nn->r_vecs[j].tx_pkts;
 			data[i++] = nn->r_vecs[j].tx_busy;
-			tmp[2] = nn->r_vecs[j].hw_csum_tx;
-			tmp[3] = nn->r_vecs[j].hw_csum_tx_inner;
-			tmp[4] = nn->r_vecs[j].tx_gather;
-			tmp[5] = nn->r_vecs[j].tx_lso;
+			tmp[3] = nn->r_vecs[j].hw_csum_tx;
+			tmp[4] = nn->r_vecs[j].hw_csum_tx_inner;
+			tmp[5] = nn->r_vecs[j].tx_gather;
+			tmp[6] = nn->r_vecs[j].tx_lso;
 		} while (u64_stats_fetch_retry(&nn->r_vecs[j].tx_sync, start));
 
 		for (k = 0; k < NN_ET_RVEC_GATHER_STATS; k++)

commit ed4ebaa85e7172e75c7bc64615a8a646ea25eef0

nfp_net_common: add usage counting of VXLAN ports
VXLAN tunnels can be established for both IPv4 and IPv6 which
means the same port may be added and removed multiple times
and we need to track the use count.  Otherwise if two tunnels
are added for the same port and one of them gets removed we
will remove the port even though the second tunnel still exists.

Fixes: 1ee3c5600ccf ("nfp_net: add RX encapsulation offload")
Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Reviewed-by: Jason S. McMullan <jason.mcmullan@netronome.com>

src/nfp_net.h
@@ -480,6 +480,7 @@ static inline bool nfp_net_fw_ver_eq(struct nfp_net_fw_version *fw_ver,
  * @tx_coalesce_usecs:      TX interrupt moderation usecs delay parameter
  * @tx_coalesce_max_frames: TX interrupt moderation frame count parameter
  * @vxlan_ports:	VXLAN ports for RX inner csum offload communicated to HW
+ * @vxlan_usecnt:	IPv4/IPv6 VXLAN port use counts
  * @qcp_cfg:            Pointer to QCP queue used for configuration notification
  * @ctrl_bar:           Pointer to mapped control BAR
  * @tx_bar:             Pointer to mapped TX queues
@@ -566,6 +567,7 @@ struct nfp_net {
 	u32 tx_coalesce_max_frames;
 
 	__be16 vxlan_ports[NFP_NET_N_VXLAN_PORTS];
+	u8 vxlan_usecnt[NFP_NET_N_VXLAN_PORTS];
 
 	u8 __iomem *qcp_cfg;
 

src/nfp_net_common.c
@@ -2264,34 +2264,55 @@ static void nfp_net_set_vxlan_port(struct nfp_net *nn, int idx, __be16 port)
 	nfp_net_reconfig(nn, NFP_NET_CFG_UPDATE_VXLAN);
 }
 
+/**
+ * nfp_net_find_vxlan_idx() - find table entry of the port or a free one
+ * @nn:   NFP Network structure
+ * @port: UDP port to look for
+ *
+ * Return: if the port is already in the table -- it's position;
+ *	   if the port is not in the table -- free position to use;
+ *	   if the table is full -- -ENOSPC.
+ */
+static int nfp_net_find_vxlan_idx(struct nfp_net *nn, __be16 port)
+{
+	int i, free_idx = -ENOSPC;
+
+	for (i = 0; i < NFP_NET_N_VXLAN_PORTS; i++) {
+		if (nn->vxlan_ports[i] == port)
+			return i;
+		if (!nn->vxlan_usecnt[i])
+			free_idx = i;
+	}
+
+	return free_idx;
+}
+
 static void nfp_net_add_vxlan_port(struct net_device *netdev,
 				   sa_family_t sa_family, __be16 port)
 {
 	struct nfp_net *nn = netdev_priv(netdev);
-	int i, free_idx = -1;
+	int idx;
 
-	for (i = 0; i < NFP_NET_N_VXLAN_PORTS; i++) {
-		if (nn->vxlan_ports[i] == port)
-			return;
-		if (!nn->vxlan_ports[i])
-			free_idx = i;
-	}
-
-	if (free_idx == -1)
+	idx = nfp_net_find_vxlan_idx(nn, port);
+	if (idx == -ENOSPC)
 		return;
 
-	nfp_net_set_vxlan_port(nn, free_idx, port);
+	if (!nn->vxlan_usecnt[idx]++)
+		nfp_net_set_vxlan_port(nn, idx, port);
 }
 
 static void nfp_net_del_vxlan_port(struct net_device *netdev,
 				   sa_family_t sa_family, __be16 port)
 {
 	struct nfp_net *nn = netdev_priv(netdev);
-	int i;
+	int idx;
 
-	for (i = 0; i < NFP_NET_N_VXLAN_PORTS; i++)
-		if (nn->vxlan_ports[i] == port)
-			nfp_net_set_vxlan_port(nn, i, 0);
+	idx = nfp_net_find_vxlan_idx(nn, port);
+	if (!nn->vxlan_usecnt[idx] || idx == -ENOSPC)
+		return;
+
+	if (!--nn->vxlan_usecnt[idx])
+		nfp_net_set_vxlan_port(nn, idx, 0);
 }
 #endif /* COMPAT__HAVE_VXLAN_OFFLOAD */
 