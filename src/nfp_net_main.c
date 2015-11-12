macris-xiao/nfp-drv-kmods-xiao

commit d1abb09669cb838e4678292920dc1043041a39ec

nfp_net_common: improve YDS-155 workaround
Memory allocation for YDS-155 workaround looks a little suspicious.
Let's move it to PF-specific code, improve the comment, add a define
and make the spare memory smaller.  Last but not least: use 64bit
write to write the dma address.

Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Acked-by: Jason S. McMullan <jason.mcmullan@netronome.com>
Reviewed-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

src/nfp_net_main.c
@@ -826,6 +826,18 @@ static int nfp_net_pci_probe(struct pci_dev *pdev,
 	 */
 	nn->me_freq_mhz = 1200;
 
+	if (nn->is_nfp3200) {
+		/* YDS-155 workaround - FW will issue DMA reads of this mem */
+		nn->spare_va = dma_zalloc_coherent(&pdev->dev,
+						   NFP3200_SPARE_DMA_SIZE,
+						   &nn->spare_dma, GFP_KERNEL);
+		if (!nn->spare_va)
+			return -ENOMEM;
+
+		nn_writeq(nn, NFP_NET_CFG_SPARE_ADDR, nn->spare_dma);
+		nn_info(nn, "Enabled NFP-3200 workaround.");
+	}
+
 	/*
 	 * Finalise
 	 */
@@ -914,6 +926,10 @@ static void nfp_net_pci_remove(struct pci_dev *pdev)
 
 	if (!nn->nfp_fallback) {
 		nfp_net_netdev_clean(nn->netdev);
+
+		if (nn->is_nfp3200)
+			dma_free_coherent(&pdev->dev, NFP3200_SPARE_DMA_SIZE,
+					  nn->spare_va, nn->spare_dma);
 
 		if (nn->msix_table)
 			iounmap(nn->msix_table);

src/nfp_net.h
@@ -121,6 +121,9 @@
 /* Offload definitions */
 #define NFP_NET_N_VXLAN_PORTS	(NFP_NET_CFG_VXLAN_SZ / sizeof(__be16))
 
+/* Workaround definitions */
+#define NFP3200_SPARE_DMA_SIZE	128
+
 /* Forward declarations */
 struct nfp_net;
 struct nfp_net_r_vector;

src/nfp_net_common.c
@@ -2546,15 +2546,6 @@ int nfp_net_netdev_init(struct net_device *netdev)
 
 	nfp_net_irqs_assign(netdev);
 
-	if (nn->is_nfp3200) {
-		/* YDS-155 workaround. */
-		nn->spare_va = dma_zalloc_coherent(&nn->pdev->dev, 4096,
-						   &nn->spare_dma, GFP_KERNEL);
-		if (!nn->spare_va)
-			return -ENOMEM;
-		nn_writel(nn, NFP_NET_CFG_SPARE_ADDR, nn->spare_dma);
-		nn_info(nn, "Enabled NFP-3200 workaround.");
-	}
 	return 0;
 
 err_reconfig:
@@ -2567,11 +2558,5 @@ err_reconfig:
  */
 void nfp_net_netdev_clean(struct net_device *netdev)
 {
-	struct nfp_net *nn = netdev_priv(netdev);
-
-	if (nn->is_nfp3200)
-		dma_free_coherent(&nn->pdev->dev, 4096,
-				  nn->spare_va, nn->spare_dma);
-
 	unregister_netdev(netdev);
 }

commit 15633c05ba6d307f87e292545ebbd4a504dc3ab9

nfp_net_main: fix handling spare DMA allocation error
When allocation of spare DMA fails we should go to the error
handling path, not just return.

Fixes: d1abb09669cb ("nfp_net_common: improve YDS-155 workaround")
Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Acked-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>
Reviewed-by: Jason S. McMullan <jason.mcmullan@netronome.com>

src/nfp_net_main.c
@@ -850,8 +850,10 @@ static int nfp_net_pci_probe(struct pci_dev *pdev,
 		nn->spare_va = dma_zalloc_coherent(&pdev->dev,
 						   NFP3200_SPARE_DMA_SIZE,
 						   &nn->spare_dma, GFP_KERNEL);
-		if (!nn->spare_va)
-			return -ENOMEM;
+		if (!nn->spare_va) {
+			err = -ENOMEM;
+			goto err_netdev_init;
+		}
 
 		nn_writeq(nn, NFP_NET_CFG_SPARE_ADDR, nn->spare_dma);
 		nn_info(nn, "Enabled NFP-3200 workaround.\n");
@@ -862,7 +864,7 @@ static int nfp_net_pci_probe(struct pci_dev *pdev,
 	 */
 	err = nfp_net_netdev_init(nn->netdev);
 	if (err)
-		goto err_netdev_init;
+		goto err_free_spare;
 
 	pci_set_drvdata(pdev, nn);
 
@@ -873,6 +875,10 @@ static int nfp_net_pci_probe(struct pci_dev *pdev,
 
 	return 0;
 
+err_free_spare:
+	if (nn->is_nfp3200)
+		dma_free_coherent(&pdev->dev, NFP3200_SPARE_DMA_SIZE,
+				  nn->spare_va, nn->spare_dma);
 err_netdev_init:
 	if (nn->msix_table)
 		iounmap(nn->msix_table);