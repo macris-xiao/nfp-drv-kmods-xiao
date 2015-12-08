macris-xiao/nfp-drv-kmods-xiao

commit 9982819b9ba4d09c5dc0039b322ec9d288db83b3

nfp_net_common: split buffer allocation into two functions
Split buffer allocation into the allocation+mapping part and the part
which puts the buffer on the rings.

Since the function which puts the buffer on the ring does the kicking
we need to stash the number of buffers allocated since last kick in
the ring structure and kick only once every BATCH times.  This is
very similar to what we do for .xmit_more.  Note that we have to
split a 32 bit index field to make sure ring still fits into a cache
line.  This should be fine unless we plan to have more than 65k rings.

Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Reviewed-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

src/nfp_net_common.c
@@ -992,33 +992,47 @@ static inline int nfp_net_rx_space(struct nfp_net_rx_ring *rx_ring)
 }
 
 /**
- * nfp_net_rx_freelist_alloc_one() - Allocate, map and write descriptors
- * @rx_ring: RX add the buffer to
+ * nfp_net_rx_alloc_one() - Allocate and map skb for RX
+ * @rx_ring:	RX ring structure of the skb
+ * @dma_addr:	Pointer to storage for DMA address (output param)
  *
- * This function will allcate a new skb, map it for DMA and write HW and SW
- * descriptors.  It will, however, *not* give the buffer to HW.
+ * This function will allcate a new skb, map it for DMA.
+ *
+ * Return: allocated skb or NULL on failure.
  */
-static int nfp_net_rx_freelist_alloc_one(struct nfp_net_rx_ring *rx_ring)
+static struct sk_buff *
+nfp_net_rx_alloc_one(struct nfp_net_rx_ring *rx_ring, dma_addr_t *dma_addr)
 {
 	struct nfp_net *nn = rx_ring->r_vec->nfp_net;
-	struct nfp_net_rx_desc *rxd;
 	struct sk_buff *skb;
-	dma_addr_t dma_addr;
-	int wr_idx;
 
 	skb = netdev_alloc_skb(nn->netdev, nn->fl_bufsz);
 	if (!skb) {
 		nn_warn_ratelimit(nn, "Failed to alloc receive SKB\n");
-		return -ENOMEM;
+		return NULL;
 	}
 
-	dma_addr = dma_map_single(&nn->pdev->dev, skb->data,
+	*dma_addr = dma_map_single(&nn->pdev->dev, skb->data,
 				  nn->fl_bufsz, DMA_FROM_DEVICE);
-	if (dma_mapping_error(&nn->pdev->dev, dma_addr)) {
+	if (dma_mapping_error(&nn->pdev->dev, *dma_addr)) {
 		dev_kfree_skb_any(skb);
 		nn_warn_ratelimit(nn, "Failed to map DMA RX buffer\n");
-		return -ENOMEM;
+		return NULL;
 	}
+
+	return skb;
+}
+
+/**
+ * nfp_net_rx_give_one() - Put mapped skb on the software and hardware rings
+ * @rx_ring:	RX ring structure
+ * @skb:	Skb to put on rings
+ * @dma_addr:	DMA address of skb mapping
+ */
+static void nfp_net_rx_give_one(struct nfp_net_rx_ring *rx_ring,
+				struct sk_buff *skb, dma_addr_t dma_addr)
+{
+	unsigned int wr_idx;
 
 	wr_idx = rx_ring->wr_p % rx_ring->cnt;
 
@@ -1027,12 +1041,20 @@ static int nfp_net_rx_freelist_alloc_one(struct nfp_net_rx_ring *rx_ring)
 	rx_ring->rxbufs[wr_idx].dma_addr = dma_addr;
 
 	/* Fill freelist descriptor */
-	rxd = &rx_ring->rxds[wr_idx];
-	rxd->fld.meta_len_dd = 0;
-	nfp_desc_set_dma_addr(&rxd->fld, dma_addr);
-	rx_ring->wr_p++;
+	rx_ring->rxds[wr_idx].fld.reserved = 0;
+	rx_ring->rxds[wr_idx].fld.meta_len_dd = 0;
+	nfp_desc_set_dma_addr(&rx_ring->rxds[wr_idx].fld, dma_addr);
 
-	return 0;
+	rx_ring->wr_p++;
+	rx_ring->wr_ptr_add++;
+	if (rx_ring->wr_ptr_add >= NFP_NET_FL_BATCH) {
+		/* Update write pointer of the freelist queue. Make
+		 * sure all writes are flushed before telling the hardware.
+		 */
+		wmb();
+		nfp_qcp_wr_ptr_add(rx_ring->qcp_fl, rx_ring->wr_ptr_add);
+		rx_ring->wr_ptr_add = 0;
+	}
 }
 
 /**
@@ -1076,25 +1098,17 @@ static void nfp_net_rx_flush(struct nfp_net_rx_ring *rx_ring)
  */
 static int nfp_net_rx_fill_freelist(struct nfp_net_rx_ring *rx_ring)
 {
+	struct sk_buff *skb;
+	dma_addr_t dma_addr;
 	int added = 0;
-	int i;
 
-	while (nfp_net_rx_space(rx_ring) >= NFP_NET_FL_BATCH) {
-		for (i = 0; i < NFP_NET_FL_BATCH; i++) {
-			if (nfp_net_rx_freelist_alloc_one(rx_ring))
-				break;
-		}
-
-		/* Update write pointer of the freelist queue. Make
-		 * sure all writes are flushed before telling the hardware.
-		 */
-		wmb();
-		nfp_qcp_wr_ptr_add(rx_ring->qcp_fl, i);
-		added += i;
-
-		/* stop on error */
-		if (i < NFP_NET_FL_BATCH)
+	while (nfp_net_rx_space(rx_ring)) {
+		skb = nfp_net_rx_alloc_one(rx_ring, &dma_addr);
+		if (!skb)
 			break;
+		nfp_net_rx_give_one(rx_ring, skb, dma_addr);
+
+		added++;
 	}
 
 	return added;

src/nfp_net.h
@@ -309,6 +309,8 @@ struct nfp_net_rx_buf {
  * @rx_qcidx:   Queue Controller Peripheral (QCP) queue index for the RX queue
  * @qcp_fl:     Pointer to base of the QCP freelist queue
  * @qcp_rx:     Pointer to base of the QCP RX queue
+ * @wr_ptr_add: Accumulated number of buffers to add to QCP write pointer
+ *              (used for free list batching)
  * @rxbufs:     Array of transmitted FL/RX buffers
  * @rxds:       Virtual address of FL/RX ring in host memory
  * @dma:        DMA address of the FL/RX ring
@@ -321,7 +323,9 @@ struct nfp_net_rx_ring {
 	u32 wr_p;
 	u32 rd_p;
 
-	u32 idx;
+	u16 idx;
+	u16 wr_ptr_add;
+
 	int fl_qcidx;
 	int rx_qcidx;
 	u8 __iomem *qcp_fl;

commit eae2ef84a2dd411c0a3e79b58a07a26590830b92

nfp_net_common: clear ring delayed kick counters
We need to clear delayed kick counters when we free rings otherwise
after ndo_close()/ndo_open() we could kick HW by more entries than
actually written to rings.

Fixes: 9982819b9ba4 ("nfp_net_common: split buffer allocation into two functions
Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Reviewed-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

src/nfp_net_common.c
@@ -1384,6 +1384,7 @@ static void nfp_net_tx_ring_free(struct nfp_net_tx_ring *tx_ring)
 	tx_ring->wr_p = 0;
 	tx_ring->rd_p = 0;
 	tx_ring->qcp_rd_p = 0;
+	tx_ring->wr_ptr_add = 0;
 
 	tx_ring->txbufs = NULL;
 	tx_ring->txds = NULL;
@@ -1458,6 +1459,7 @@ static void nfp_net_rx_ring_free(struct nfp_net_rx_ring *rx_ring)
 	rx_ring->cnt = 0;
 	rx_ring->wr_p = 0;
 	rx_ring->rd_p = 0;
+	rx_ring->wr_ptr_add = 0;
 
 	rx_ring->rxbufs = NULL;
 	rx_ring->rxds = NULL;