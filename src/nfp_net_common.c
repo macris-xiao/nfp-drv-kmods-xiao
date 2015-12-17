macris-xiao/nfp-drv-kmods-xiao

commit 9480ccd0da8e28d55c20df1492cdd8a0df9ae70a

nfp_net_common: remove unnecessary netif_carrier_off() from .ndo_open
Carrier must be off if the device is no up, if we need to disable
it in .ndo_open, then we have a bug and should fix it instead of
doing blind workarounds.

Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Reviewed-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

src/nfp_net_common.c
@@ -1750,9 +1750,6 @@ static int nfp_net_netdev_open(struct net_device *netdev)
 		return -EBUSY;
 	}
 
-	netif_carrier_off(netdev);
-	nn->link_up = false;
-
 	new_ctrl = nn->ctrl;
 
 	/* Step 1: Allocate resources for rings and the like

commit 1b4ed6c38a7c6eab0381bc3853049ca7e7ca37c9

nfp_net_common: call netif_carrier_off() during init
Netdevs default to carrier on, we should call netif_carrier_off()
during initialization since we handle carrier state changes in the
driver.

Fixes: 9480ccd0da8e ("nfp_net_common: remove unnecessary netif_carrier_off() from .ndo_open")
Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
Reviewed-by: Rolf Neugebauer <rolf.neugebauer@netronome.com>

src/nfp_net_common.c
@@ -2450,6 +2450,7 @@ int nfp_net_netdev_init(struct net_device *netdev)
 	ether_setup(netdev);
 	netdev->netdev_ops = &nfp_net_netdev_ops;
 	netdev->watchdog_timeo = msecs_to_jiffies(5 * 1000);
+	netif_carrier_off(netdev);
 
 	nfp_net_set_ethtool_ops(netdev);
 	nfp_net_irqs_assign(netdev);