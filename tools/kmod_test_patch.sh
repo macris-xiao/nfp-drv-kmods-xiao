346a6cdb7d1167b395b62e24d2673fbd0996ec60
Jakub Kicinski <jakub.kicinski@netronome.com>
2016-12-02 14:37:34 +0000

    main: move merged code into nfp_main.c
    After modules were all merged together we can move shared code
    to nfp_main.c.  No functional changes.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Dinan Gunawardena <dinan.gunawardena@netronome.com>

--- tools/kmod_test_patch.sh
+++ tools/kmod_test_patch.sh
@@ -40,7 +40,7 @@ REPO_URL=ssh://hg.netronome.com/data/git/repos/nfp-drv-kmods.git
 # Counts of warnings which already exist in your code (e.g. false positives)
 INCUMBENT_SPARSE_WARNINGS=71
 INCUMBENT_COCCI_WARNINGS=8
-INCUMBENT_KDOC_WARNINGS=25
+INCUMBENT_KDOC_WARNINGS=24
 INCUMBENT_NEWLINE_WARNINGS=61
 # Default compiler to use for most testing (must be something conservative
 # otherwise the old kernels won't build).  Check if gcc-4.9, gcc-4.8 or gcc
