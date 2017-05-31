76e5c1ff83c9d01b169a5582184163224237d24e
Jakub Kicinski <jakub.kicinski@netronome.com>
2017-05-31 15:32:50 -0700

    Revert "beginning to scratch the surface on removing 3200 and adding 3800"
    No review.
    
    This reverts commit 9ef3d9c89bbe36ca5d5fe7d4d1d39f67b060ac5d.

--- src/nfpcore/nfp_cpp.h
+++ src/nfpcore/nfp_cpp.h
@@ -229,11 +229,12 @@ static inline int NFP_CPP_STEPPING_decode(const char *_str_major_minor)
  * Return:      true if model is in the NFP6000 family, false otherwise.
  */
 #define NFP_CPP_MODEL_IS_6000(model)		     \
-	(NFP_CPP_MODEL_CHIP_of(model) == 0x6200)
+	((0x4000 <= NFP_CPP_MODEL_CHIP_of(model)) && \
+	 (NFP_CPP_MODEL_CHIP_of(model) < 0x7000))
 
 /* NFP Interface types - logical interface for this CPP connection
  * 4 bits are reserved for interface type.
- */ 
+ */
 #define NFP_CPP_INTERFACE_TYPE_INVALID      0x0
 #define NFP_CPP_INTERFACE_TYPE_PCI          0x1
 #define NFP_CPP_INTERFACE_TYPE_ARM          0x2
