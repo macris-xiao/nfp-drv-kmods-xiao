42d21c4060aefcd179782f1db306992644c4d29f
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-02-13 15:52:12 -0800

    nfp: bpf: add helper for basic map call checks
    Add a verifier helper for performing the basic state checks
    before a call to a map helper.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Reviewed-by: Quentin Monnet <quentin.monnet@netronome.com>
    Reviewed-by: Jiong Wang <jiong.wang@netronome.com>

--- tools/kmod_test_patch.sh
+++ tools/kmod_test_patch.sh
@@ -431,6 +431,7 @@ done
 		/? ".*" *: ".*"/d;
 		/debugfs_create_/d;
 		/NL_SET_ERR_MSG_MOD/d;
+		/nfp_bpf_[a-z_]*_ok/d;
 		/_rtsym.*(/d;
 		/\.[a-z0-9_]*[ \t]*= ".*",/d' | wc -l)
 	    check_warn_cnt $nl_warnings $INCUMBENT_NEWLINE_WARNINGS "'line endings in strings'"
