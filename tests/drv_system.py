55f108bb07fe1f119fb66c4c175c0f8923a31a83
Jakub Kicinski <jakub.kicinski@netronome.com>
2018-05-21 16:22:25 -0700

    tests: fix build name extraction after toolchain changes
    Commit 53d466b21bc1 ("tests: update MIP name regex to account for version
    and build number") assumed build number is always there.  Fix the regexp
    in case it's not.
    
    Signed-off-by: Jakub Kicinski <jakub.kicinski@netronome.com>

--- tests/drv_system.py
+++ tests/drv_system.py
@@ -408,7 +408,7 @@ class DrvSystem(System):
 
     def __get_mip_info(self, fwpath):
         _, out = self.cmd('readelf -p .note.build_info ' + fwpath)
-        return re.search('Name: ([^^]{1,15})(\^JVersion: (.*)\^JBuild Number: (.*)\^J)?\n', out).groups()
+        return re.search('Name: ([^^]{1,15})(\^JVersion: (.*)\^JBuild Number: ?(.*)(\^J)?)?\n', out).groups()
 
     def get_mip_name(self, fwpath):
         return self.__get_mip_info(fwpath)[0]
