2e9aa90065d7ef889e35087ede3e4ab51c6b755c
Quentin Monnet <quentin.monnet@netronome.com>
2018-10-05 15:50:13 +0100

    tests: ebpf: add a test to check limit of stack size (passing)
    This program should require 512 bytes of stack in total, for host XDP as
    for offloaded XDP. This is the current limit of the stack offered by the
    firmware, so we should accept it for offload.
    
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>

--- tests/ebpf/tests.py
+++ tests/ebpf/tests.py
@@ -468,6 +468,8 @@ class NFPKmodBPF(NFPKmodAppGrp):
                 'BPF-to-BPF function call with unaligned u8 stack read/write'),
                ('xdp_function_call_stack_frame_size', XDPfunctionCall,
                 'BPF-to-BPF call with checks on stack frame size alignment'),
+               ('xdp_function_call_stack_limit_pass', XDPfunctionCall,
+                'BPF-to-BPF call allowed iff R6-R9 saving is optimised out'),
         )
 
         for i in (0, 1, 3, 7, 8, 11):
