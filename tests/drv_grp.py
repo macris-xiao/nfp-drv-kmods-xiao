592aadef803d003c200a5a626d460989de88f252
Quentin Monnet <quentin.monnet@netronome.com>
2018-11-02 16:40:41 +0000

    tests: replace IP addresses of DUT instead of adding them
    When setting IP address (in particular, IPv6), "ip addr add" returns an
    error if the DUT already has the correct address, which stops the
    execution of the tests.
    
    Fix this by using "ip addr replace" instead for both v4 and v6
    addresses.
    
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Acked-by: Jakub Kicinski <jakub.kicinski@netronome.com>

--- tests/drv_grp.py
+++ tests/drv_grp.py
@@ -429,8 +429,8 @@ class NFPKmodAppGrp(NFPKmodGrp):
         # Init DUT
         for p in range(len(self.eth_x)):
             cmd += 'ip link set dev {ifc} promisc on up && '
-            cmd += 'ip addr add dev {ifc} {ipv4} && '
-            cmd += 'ip addr add dev {ifc} {ipv6} && '
+            cmd += 'ip addr replace dev {ifc} {ipv4} && '
+            cmd += 'ip addr replace dev {ifc} {ipv6} ; '
             cmd = cmd.format(ifc=self.eth_x[p],
                              ipv4=self.addr_x[p], ipv6=self.addr_v6_x[p])
         cmd += 'true'
