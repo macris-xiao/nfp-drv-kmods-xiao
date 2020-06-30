Netronome/nfp-drv-kmods

commit 5579ca0d1748eb6917d891b1d55b8ec801db2939

nfp: update indirect block support
commit 50c1b1c9385fbb35c25b27608e00bcf89368e8ba upstream.

Register ndo callback via flow_indr_dev_register() and
flow_indr_dev_unregister().

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
Signed-off-by: David S. Miller <davem@davemloft.net>
[simon: reworked for compatibility with older kernels]
Signed-off-by: Simon Horman <simon.horman@netronome.com>
Signed-off-by: Louis Peens <louis.peens@netronome.com>

src/flower/main.h
@@ -468,11 +468,16 @@ int nfp_flower_setup_qos_offload(struct nfp_app *app, struct net_device *netdev,
 				 struct tc_cls_matchall_offload *flow);
 void nfp_flower_stats_rlim_reply(struct nfp_app *app, struct sk_buff *skb);
 #endif
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0) && \
+    LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
 int nfp_flower_reg_indir_block_handler(struct nfp_app *app,
 				       struct net_device *netdev,
 				       unsigned long event);
 #endif
+int nfp_flower_indr_setup_tc_cb(struct net_device *netdev, void *cb_priv,
+                               enum tc_setup_type type, void *type_data);
+int nfp_flower_setup_indr_block_cb(enum tc_setup_type type, void *type_data,
+                                  void *cb_priv);
 
 void
 __nfp_flower_non_repr_priv_get(struct nfp_flower_non_repr_priv *non_repr_priv);

src/flower/main.c
@@ -847,6 +847,12 @@ static int nfp_flower_init(struct nfp_app *app)
 	if (err)
 		goto err_cleanup;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)
+	err = flow_indr_dev_register(nfp_flower_indr_setup_tc_cb, app);
+	if (err)
+		goto err_cleanup;
+#endif
+
 	if (app_priv->flower_ext_feats & NFP_FL_FEATS_VF_RLIM)
 		nfp_flower_qos_init(app);
 
@@ -872,6 +878,11 @@ static void nfp_flower_clean(struct nfp_app *app)
 	skb_queue_purge(&app_priv->cmsg_skbs_high);
 	skb_queue_purge(&app_priv->cmsg_skbs_low);
 	flush_work(&app_priv->cmsg_work);
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)
+	flow_indr_dev_unregister(nfp_flower_indr_setup_tc_cb, app,
+				 nfp_flower_setup_indr_block_cb);
+#endif
 
 	if (app_priv->flower_ext_feats & NFP_FL_FEATS_VF_RLIM)
 		nfp_flower_qos_cleanup(app);
@@ -976,7 +987,8 @@ nfp_flower_netdev_event(struct nfp_app *app, struct net_device *netdev,
 			return ret;
 	}
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0) && \
+    LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
 	ret = nfp_flower_reg_indir_block_handler(app, netdev, event);
 	if (ret & NOTIFY_STOP_MASK)
 		return ret;

src/flower/offload.c
@@ -2025,8 +2025,8 @@ nfp_flower_indr_block_cb_priv_lookup(struct nfp_app *app,
 	return NULL;
 }
 
-static int nfp_flower_setup_indr_block_cb(enum tc_setup_type type,
-					  void *type_data, void *cb_priv)
+int nfp_flower_setup_indr_block_cb(enum tc_setup_type type,
+				   void *type_data, void *cb_priv)
 {
 	struct nfp_flower_indr_block_cb_priv *priv = cb_priv;
 	compat__flow_cls_offload *flower = type_data;
@@ -2143,10 +2143,13 @@ nfp_flower_setup_indr_tc_block(struct net_device *netdev, struct nfp_app *app,
 	return 0;
 }
 
-static int
-nfp_flower_indr_setup_tc_cb(struct net_device *netdev, void *cb_priv,
-			    enum tc_setup_type type, void *type_data)
+int nfp_flower_indr_setup_tc_cb(struct net_device *netdev, void *cb_priv,
+				enum tc_setup_type type, void *type_data)
 {
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
+	if (!nfp_fl_is_netdev_to_offload(netdev))
+		return -EOPNOTSUPP;
+#endif
 	switch (type) {
 	case TC_SETUP_BLOCK:
 		return nfp_flower_setup_indr_tc_block(netdev, cb_priv,
@@ -2156,6 +2159,7 @@ nfp_flower_indr_setup_tc_cb(struct net_device *netdev, void *cb_priv,
 	}
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0)
 int nfp_flower_reg_indir_block_handler(struct nfp_app *app,
 				       struct net_device *netdev,
 				       unsigned long event)
@@ -2181,4 +2185,5 @@ int nfp_flower_reg_indir_block_handler(struct nfp_app *app,
 
 	return NOTIFY_OK;
 }
-#endif
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(5, 8, 0) */
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0) */

macris-xiao/nfp-drv-kmods-xiao

commit 420921190913766777ca6e0fa9891e96cb569f93

nfp: correct whitespace around indr block function declarations
This corrects a miss-backport of an upstream patch.
It is cosmetic only and is not intended to introduce any run-time changes.

Fixes: 5579ca0d1748 ("nfp: update indirect block support")
Signed-off-by: Simon Horman <simon.horman@netronome.com>
Signed-off-by: Louis Peens <louis.peens@netronome.com>

src/flower/main.h
@@ -475,9 +475,9 @@ int nfp_flower_reg_indir_block_handler(struct nfp_app *app,
 				       unsigned long event);
 #endif
 int nfp_flower_indr_setup_tc_cb(struct net_device *netdev, void *cb_priv,
-                               enum tc_setup_type type, void *type_data);
+				enum tc_setup_type type, void *type_data);
 int nfp_flower_setup_indr_block_cb(enum tc_setup_type type, void *type_data,
-                                  void *cb_priv);
+				   void *cb_priv);
 
 void
 __nfp_flower_non_repr_priv_get(struct nfp_flower_non_repr_priv *non_repr_priv);