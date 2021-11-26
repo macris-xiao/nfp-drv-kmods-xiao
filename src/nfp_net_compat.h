fb70fc6f97b8a75b9d13a98a5cfedeb833e37431
Yu Xiao <yu.xiao@corigine.com>
2021-11-26 04:51:06 +0200

    nfp: compat: adding compat code for RHEL 8.3
    RHEL 8.3 provides `nfp_flower_indr_setup_tc_cb`, so providing a
    compat version to use `compat__nfp_flower_indr_setup_tc_cb` in
    RHEL 8.3.
    
    Disable support of `CONFIG_NFP_APP_FLOWER` to solve the error:
    (nfp: unknown FW app ID 0x03, driver too old or support FW not built in)
    
    Signed-off-by: Yu Xiao <yu.xiao@corigine.com>
    Signed-off-by: Yinjun Zhang <yinjun.zhang@corigine.com>

--- src/nfp_net_compat.h
+++ src/nfp_net_compat.h
@@ -1423,14 +1423,14 @@ struct flow_block_cb {};
 
 #if VER_NON_RHEL_GE(5, 0) || VER_RHEL_GE(8, 0)
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
+#if VER_NON_RHEL_GE(5, 9)
 #define compat__nfp_flower_indr_setup_tc_cb nfp_flower_indr_setup_tc_cb
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)
+#elif VER_NON_RHEL_GE(5, 8) || RHEL_RELEASE_GE(8, 305, 0, 0)
 int compat__nfp_flower_indr_setup_tc_cb(struct net_device *netdev,
 					void *cb_priv, enum tc_setup_type type,
 					void *type_data, void *data,
 					void (*cleanup)(struct flow_block_cb *block_cb));
-#else
+#elif VER_NON_RHEL_LT(5, 8) || VER_RHEL_GE(8, 3)
 int compat__nfp_flower_indr_setup_tc_cb(struct net_device *netdev,
 					void *cb_priv, enum tc_setup_type type,
 					void *type_data);
