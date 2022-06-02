290618ee75bf0204ae9e6ffb042c753e00fef345
Yu Xiao <yu.xiao@corigine.com>
2022-06-02 05:48:54 +0200

    nfp: ethtool: support reporting link modes
    Add support for reporting link modes,
    including `Supported link modes` and `Advertised link modes`,
    via ethtool $DEV.
    
    A new command `SPCODE_READ_MEDIA` is added to read info from
    management firmware. Also, the mapping table `nfp_eth_media_table`
    associates the link modes between NFP and kernel. Both of them
    help to support this ability.
    
    Signed-off-by: Yu Xiao <yu.xiao@corigine.com>

--- src/nfp_net_compat.h
+++ src/nfp_net_compat.h
@@ -574,7 +574,7 @@ static inline int skb_inner_transport_offset(const struct sk_buff *skb)
 }
 #endif
 
-#if VER_NON_RHEL_LT(4, 6) || VER_RHEL_LT(7, 5)
+#if VER_NON_RHEL_LT(4, 6) || VER_RHEL_LT(7, 3)
 struct compat__ethtool_link_ksettings {
 	struct ethtool_cmd base;
 };
