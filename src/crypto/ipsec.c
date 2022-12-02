811d622f2a4e22ef42de8f28b900c756a0adc561
Leon Romanovsky <leonro@nvidia.com>
2022-12-02 20:41:28 +0200

    xfrm: allow state packet offload mode
    Allow users to configure xfrm states with packet offload mode.
    The packet mode must be requested both for policy and state, and
    such requires us to do not implement fallback.
    
    We explicitly return an error if requested packet mode can't
    be configured.
    
    Reviewed-by: Raed Salem <raeds@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Yu Xiao <yu.xiao@corigine.com>

--- src/crypto/ipsec.c
+++ src/crypto/ipsec.c
@@ -302,6 +302,13 @@ static int nfp_net_xfrm_add_state(struct xfrm_state *x)
 		return -EINVAL;
 	}
 
+#if VER_NON_RHEL_GE(6, 2)
+	if (x->xso.type != XFRM_DEV_OFFLOAD_CRYPTO) {
+		nn_err(nn, "Unsupported xfrm offload tyoe\n");
+		return -EINVAL;
+	}
+#endif
+
 	cfg->spi = ntohl(x->id.spi);
 
 	/* Hash/Authentication */
